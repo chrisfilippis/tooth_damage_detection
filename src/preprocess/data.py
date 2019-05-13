15d4b71e5ae1a66bb2c4f4d8fdcfa9e607e0990d
Chris Filippis <chris.filippis@vodafone.com>
2019-05-14 02:02:54 +0300

    feat: add read-roi package

---
+++ src/preprocess/data.py
@@ -0,0 +1,11 @@
+from read_roi import read_roi_file
+from read_roi import read_roi_zip
+
+roi_file_path = ''
+roi_zip_path = ''
+
+roi = read_roi_file(roi_file_path)
+
+# or
+
+rois = read_roi_zip(roi_zip_path)
